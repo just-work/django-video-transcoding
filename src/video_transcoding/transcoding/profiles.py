35bef673478842d1cb6490eca3c4b937983d0582
tumb1er <zimbler@gmail.com>
2024-06-25 15:58:48 +0300

    #40 default preset defined

--- src/video_transcoding/transcoding/profiles.py
+++ src/video_transcoding/transcoding/profiles.py
@@ -1,9 +1,15 @@
 from dataclasses import dataclass
 from typing import List
 
+from fffw.graph import VideoMeta, AudioMeta
+
 
 @dataclass
 class VideoTrack:
+    """
+    Settings for a single video stream in resulting media file
+    """
+    id: str
     codec: str
     constant_rate_factor: int
     preset: str
@@ -20,6 +26,10 @@ class VideoTrack:
 
 @dataclass
 class AudioTrack:
+    """
+    Settings for a single audio stream in resulting media file
+    """
+    id: str
     codec: str
     bitrate: int
     channels: int
@@ -27,19 +37,106 @@ class AudioTrack:
 
 
 @dataclass
-class Condition:
+class VideoCondition:
+    """
+    Condition for source video stream for video profile selection
+    """
     min_width: int
     min_height: int
     min_bitrate: int
-    min_frame_rate: int
-    sample_rate: int
+    min_frame_rate: float
+
+    def is_valid(self, meta: VideoMeta) -> bool:
+        """
+        :param meta: source video stream metadata
+        :return: True if source stream satisfies condition.
+        """
+        return (
+            meta.width >= self.min_width and
+            meta.height >= self.min_height and
+            meta.bitrate >= self.min_bitrate and
+            meta.frame_rate >= self.min_frame_rate
+        )
+
+
+@dataclass
+class AudioCondition:
+    """
+    Condition for source audio stream for video profile selection
+    """
+
+    min_sample_rate: int
+    min_bitrate: int
+
+    def is_valid(self, meta: AudioMeta) -> bool:
+        """
+        :param meta: source video stream metadata
+        :return: True if source stream satisfies condition.
+        """
+        return (
+            meta.bitrate >= self.min_bitrate and
+            meta.sampling_rate >= self.min_sample_rate
+        )
+
+
+@dataclass
+class VideoProfile:
+    """
+    Video transcoding profile.
+    """
+    condition: VideoCondition
+    video: List[str]  # List of VideoTrack ids defined in a preset
+
+
+@dataclass
+class AudioProfile:
+    """
+    Audio transcoding profile.
+    """
+    condition: AudioCondition
+    audio: List[str]  # List of AudioTrack ids defined in a preset
 
 
 @dataclass
 class Profile:
-    condition: Condition
+    """
+    Selected transcoding profile containing a number of audio and video streams.
+    """
     video: List[VideoTrack]
     audio: List[AudioTrack]
+
+
+@dataclass
+class Preset:
+    """
+    A set of video and audio profiles to select from.
+    """
+    video_profiles: List[VideoProfile]
+    audio_profiles: List[AudioProfile]
+    video: List[VideoTrack]
+    audio: List[AudioTrack]
+
+    def select_profile(self, video: VideoMeta, audio: AudioMeta) -> Profile:
+        video_profile = None
+        for vp in self.video_profiles:
+            if vp.condition.is_valid(video):
+                video_profile = vp
+                break
+        if video_profile is None:
+            raise RuntimeError("No compatible video profiles")
+
+        audio_profile = None
+        for ap in self.audio_profiles:
+            if ap.condition.is_valid(audio):
+                audio_profile = ap
+                break
+        if audio_profile is None:
+            raise RuntimeError("No compatible audio profiles")
+
+        return Profile(
+            video=[v for v in self.video if v.id in video_profile.video],
+            audio=[a for a in self.audio if a.id in audio_profile.audio],
+        )
 
 
 # Default frame rate
@@ -51,16 +148,65 @@ GOP_DURATION = 2
 # Force key frame every N seconds
 KEY_FRAMES = 'expr:if(isnan(prev_forced_t),1,gte(t,prev_forced_t+{sec}))'
 
-DEFAULT_PROFILE = Profile(
-    condition=Condition(
-        min_width=0,
-        min_height=0,
-        min_bitrate=0,
-        min_frame_rate=0,
-        sample_rate=0,
-    ),
+DEFAULT_PRESET = Preset(
+    video_profiles=[
+        VideoProfile(
+            condition=VideoCondition(
+                min_width=1920,
+                min_height=1080,
+                min_bitrate=4_000_000,
+                min_frame_rate=0,
+            ),
+            video=['1080p', '720p', '480p', '360p']
+        ),
+        VideoProfile(
+            condition=VideoCondition(
+                min_width=1280,
+                min_height=720,
+                min_bitrate=2_500_000,
+                min_frame_rate=0,
+            ),
+            video=['720p', '480p', '360p']
+        ),
+        VideoProfile(
+            condition=VideoCondition(
+                min_width=854,
+                min_height=480,
+                min_bitrate=1_200_000,
+                min_frame_rate=0,
+            ),
+            video=['480p', '360p']
+        ),
+        VideoProfile(
+            condition=VideoCondition(
+                min_width=0,
+                min_height=0,
+                min_bitrate=0,
+                min_frame_rate=0,
+            ),
+            video=['360p']
+        ),
+    ],
+    audio_profiles=[
+        AudioProfile(
+            condition=AudioCondition(
+                min_bitrate=160,
+                min_sample_rate=0
+            ),
+            audio=['192k', '96k']
+        ),
+        AudioProfile(
+            condition=AudioCondition(
+                min_bitrate=0,
+                min_sample_rate=0
+            ),
+            audio=['96k']
+        ),
+    ],
+
     video=[
         VideoTrack(
+            id='1080p',
             codec='libx264',
             profile='high',
             preset='slow',
@@ -75,6 +221,7 @@ DEFAULT_PROFILE = Profile(
             frame_rate=FRAME_RATE,
         ),
         VideoTrack(
+            id='720p',
             codec='libx264',
             profile='high',
             preset='slow',
@@ -89,6 +236,7 @@ DEFAULT_PROFILE = Profile(
             frame_rate=FRAME_RATE,
         ),
         VideoTrack(
+            id='480p',
             codec='libx264',
             profile='medium',
             preset='slow',
@@ -103,6 +251,7 @@ DEFAULT_PROFILE = Profile(
             frame_rate=FRAME_RATE,
         ),
         VideoTrack(
+            id='360p',
             codec='libx264',
             profile='medium',
             preset='slow',
@@ -116,16 +265,17 @@ DEFAULT_PROFILE = Profile(
             gop_size=GOP_DURATION * FRAME_RATE,
             frame_rate=FRAME_RATE,
         ),
-
     ],
     audio=[
         AudioTrack(
+            id='192k',
             codec='aac',
             bitrate=192000,
             channels=2,
             sample_rate=48000,
         ),
         AudioTrack(
+            id='96k',
             codec='aac',
             bitrate=96000,
             channels=2,
