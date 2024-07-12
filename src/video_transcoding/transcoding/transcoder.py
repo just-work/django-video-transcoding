66248699c7fad6829af06061d510a4f7fc92ea34
tumb1er <zimbler@gmail.com>
2024-07-12 13:15:44 +0300

    #279 transcoder is now a building block

--- src/video_transcoding/transcoding/transcoder.py
+++ src/video_transcoding/transcoding/transcoder.py
@@ -1,44 +1,33 @@
-import dataclasses
-from pprint import pformat
+from typing import List
 
-from fffw.encoding import FFMPEG, input_file, Stream, Scale, output_file
+from fffw import encoding
 from fffw.encoding.vector import SIMD, Vector
-from fffw.graph import VIDEO, AUDIO
 
+from video_transcoding import defaults
 from video_transcoding.transcoding import codecs
 from video_transcoding.transcoding.metadata import Metadata, Analyzer
-from video_transcoding.transcoding.profiles import Preset, Profile
+from video_transcoding.transcoding.profiles import Profile
 from video_transcoding.utils import LoggerMixin
-
-# Allowed duration difference between source and result
-DURATION_DELTA = 0.95
-
-
-class TranscodeError(Exception):
-    """ Video transcoding error."""
-
-    def __init__(self, message: str):
-        self.message = message
-        super().__init__(message)
 
 
 class Transcoder(LoggerMixin):
-    """ Video transcoder.
-
-    >>> t = Transcoder('http://source.localhost/source.mp4', '/tmp/result.mp4')
-    >>> t.transcode()
+    """
+    Source transcoding logic.
     """
 
-    def __init__(self, source: str, destination: str, preset: Preset):
-        """
-        :param source: source file link (http/ftp or file path)
-        :param destination: result file path
-        :param preset: transcoding preset
-        """
+    def __init__(self, src: str, dst: str, profile: Profile):
         super().__init__()
-        self.source = source
-        self.destination = destination
-        self.preset = preset
+        self.src = src
+        self.dst = dst
+        self.profile = profile
+
+    def __call__(self) -> Metadata:
+        """
+        Performs source file processing.
+
+        :return: result metadata
+        """
+        return self.process()
 
     def get_media_info(self, filename: str) -> Metadata:
         """
@@ -47,99 +36,22 @@ class Transcoder(LoggerMixin):
         :param filename: analyzed media
         :returns: metadata object with video and audio stream
         """
-        audios, videos = Analyzer().get_meta_data(filename)
+        self.logger.debug("Analyzing %s", filename)
+        mi = Analyzer().get_meta_data(filename)
+        if not mi.videos:
+            raise ValueError("missing video stream")
+        if not mi.audios:
+            raise ValueError("missing audio stream")
+        return mi
 
-        if videos is None:
-            raise TranscodeError("missing video stream")
-        if audios is None:
-            raise TranscodeError("missing audio stream")
-        media_info = Metadata(videos=videos, audios=audios)
-        self.logger.info("Parsed media info:\n%s",
-                         pformat(dataclasses.asdict(media_info)))
-        return media_info
-
-    def transcode(self) -> Metadata:
-        """ Transcodes video
-
-        * checks source mediainfo
-        * runs `ffmpeg`
-        * validates result
-
-        :returns: metadata with all video and audio stream in resulting file
-        """
-        # Get source mediainfo to use in validation
-        src = self.get_media_info(self.source)
-
-        # Select transcoding profile from source metadata
-        profile = self.select_profile(src)
-
-        # Initialize source file descriptor with stream metadata
-        source = input_file(self.source,
-                            Stream(VIDEO, src.video),
-                            Stream(AUDIO, src.audio))
-
-        # Initialize output file with audio and codecs from profile tracks.
-        video_tracks = []
-        for video in profile.video:
-            video_tracks.append(codecs.VideoCodec(
-                codec=video.codec,
-                force_key_frames=video.force_key_frames,
-                constant_rate_factor=video.constant_rate_factor,
-                preset=video.preset,
-                max_rate=video.max_rate,
-                buf_size=video.buf_size,
-                profile=video.profile,
-                pix_fmt=video.pix_fmt,
-                gop=video.gop_size,
-                rate=video.frame_rate,
-            ))
-        audio_tracks = []
-        for audio in profile.audio:
-            audio_tracks.append(codecs.AudioCodec(
-                codec=audio.codec,
-                bitrate=audio.bitrate,
-                channels=audio.channels,
-                rate=audio.sample_rate,
-            ))
-        dst = output_file(self.destination,
-                          *video_tracks,
-                          *audio_tracks,
-                          format='mp4')
-
-        # ffmpeg wrapper with vectorized processing capabilities
-        simd = SIMD(source, dst,
-                    overwrite=True, loglevel='repeat+level+info')
-
-        # per-video-track scaling
-        scaling_params = [
-            (video.width, video.height) for video in profile.video
-        ]
-        scaled_video = simd.video.connect(Scale, params=scaling_params)
-
-        # connect scaled video streams to simd video codecs
-        scaled_video | Vector(video_tracks)
-
-        # pass audio as is to simd audio codecs
-        simd.audio | Vector(audio_tracks)
-
-        # Run ffmpeg
-        self.run(simd.ffmpeg)
-
-        # Get result mediainfo
-        dest_media_info = self.get_media_info(self.destination)
-
-        # Validate ffmpeg result
-        self.validate(src, dest_media_info)
-
-        return dest_media_info
-
-    def select_profile(self, src: Metadata) -> Profile:
-        """
-        :param src: source metadata
-        :return: selected transcoding profile
-        """
-        profile = self.preset.select_profile(src.video, src.audio)
-        return profile
+    @staticmethod
+    def run(ff: encoding.FFMPEG) -> None:
+        """ Starts ffmpeg process and captures errors from it's logs"""
+        return_code, output, error = ff.run()
+        if return_code != 0:
+            # Check return code and error messages
+            error = error or f"invalid ffmpeg return code {return_code}"
+            raise RuntimeError(error)
 
     @staticmethod
     def validate(source_media_info: Metadata,
@@ -154,17 +66,93 @@ class Transcoder(LoggerMixin):
                            source_media_info.audio.duration)
         dst_duration = min(dest_media_info.video.duration,
                            dest_media_info.audio.duration)
-        if dst_duration < DURATION_DELTA * src_duration:
+        if dst_duration < defaults.VIDEO_DURATION_TOLERANCE * src_duration:
             # Check whether result duration corresponds to source duration
             # (damaged source files may be processed successfully but result
             # is shorter)
-            raise TranscodeError(f"incomplete file: {dst_duration}")
+            raise RuntimeError(f"incomplete file: {dst_duration}")
+
+    def process(self) -> Metadata:
+        src = self.get_media_info(self.src)
+        ff = self.prepare_ffmpeg(src)
+        self.run(ff)
+        # Get result mediainfo
+        dst = self.get_media_info(self.dst)
+        # Validate ffmpeg result
+        self.validate(src, dst)
+        return dst
+
+    def prepare_ffmpeg(self, src: Metadata) -> encoding.FFMPEG:
+        """
+        Prepares ffmpeg command for a given source
+        :param src: input file metadata
+        :return: ffmpeg wrapper
+        """
+        # Initialize source file descriptor with stream metadata
+        source = self.prepare_input(src)
+
+        # Initialize output file with audio and codecs from profile tracks.
+        video_codecs = self.prepare_video_codecs()
+        audio_codecs = self.prepre_audio_codecs()
+        dst = self.prepare_output(audio_codecs, video_codecs)
+
+        # ffmpeg wrapper with vectorized processing capabilities
+        simd = SIMD(source, dst,
+                    overwrite=True, loglevel='repeat+level+info')
+
+        # per-video-track scaling
+        scaling_params = [
+            (video.width, video.height) for video in self.profile.video
+        ]
+        scaled_video = simd.video.connect(encoding.Scale, params=scaling_params)
+
+        # connect scaled video streams to simd video codecs
+        scaled_video | Vector(video_codecs)
+
+        # pass audio as is to simd audio codecs
+        simd.audio | Vector(audio_codecs)
+
+        return simd.ffmpeg
 
     @staticmethod
-    def run(ff: FFMPEG) -> None:
-        """ Starts ffmpeg process and captures errors from it's logs"""
-        return_code, output, error = ff.run()
-        if return_code != 0:
-            # Check return code and error messages
-            error = error or f"invalid ffmpeg return code {return_code}"
-            raise TranscodeError(error)
+    def prepare_input(src: Metadata) -> encoding.Input:
+        return encoding.input_file(src.uri, *src.streams)
+
+    def prepare_output(self,
+                       audio_codecs: List[encoding.AudioCodec],
+                       video_codecs: List[encoding.VideoCodec],
+                       ) -> encoding.Output:
+        return encoding.output_file(
+            self.dst,
+            *video_codecs,
+            *audio_codecs,
+            format=self.profile.container.format
+        )
+
+    def prepre_audio_codecs(self) -> List[codecs.AudioCodec]:
+        audio_codecs = []
+        for audio in self.profile.audio:
+            audio_codecs.append(codecs.AudioCodec(
+                codec=audio.codec,
+                bitrate=audio.bitrate,
+                channels=audio.channels,
+                rate=audio.sample_rate,
+            ))
+        return audio_codecs
+
+    def prepare_video_codecs(self) -> List[codecs.VideoCodec]:
+        video_codecs = []
+        for video in self.profile.video:
+            video_codecs.append(codecs.VideoCodec(
+                codec=video.codec,
+                force_key_frames=video.force_key_frames,
+                constant_rate_factor=video.constant_rate_factor,
+                preset=video.preset,
+                max_rate=video.max_rate,
+                buf_size=video.buf_size,
+                profile=video.profile,
+                pix_fmt=video.pix_fmt,
+                gop=video.gop_size,
+                rate=video.frame_rate,
+            ))
+        return video_codecs
