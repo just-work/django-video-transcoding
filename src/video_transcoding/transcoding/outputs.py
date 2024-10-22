32a8c498d1ca421d1f0c4884978bcf49181c664b
tumb1er <zimbler@gmail.com>
2024-10-22 17:16:35 +0300

    Introduce segment muxer wrapper

--- src/video_transcoding/transcoding/outputs.py
+++ src/video_transcoding/transcoding/outputs.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Dict, Any
 
 from fffw.encoding.outputs import Output
 from fffw.wrapper import param
@@ -14,6 +14,28 @@ class HLSOutput(Output):
     master_pl_name: Optional[str] = None
     muxdelay: Optional[str] = None
     copyts: bool = param(default=False)
+
+
+def render_opts(value: Dict[str, Any]) -> str:
+    """
+    Formatter for segment_format_options option for segment muxer.
+
+    > -segment_format_options <dictionary> ... set list of options for the container format used for the segments
+    """
+    return ':'.join(f'{k}={v}' for k, v in value.items())
+
+
+@dataclass
+class SegmentOutput(Output):
+    """
+    Segment muxer
+    """
+    segment_format: Optional[str] = None
+    segment_format_options: Optional[dict] = param(render=render_opts)
+    segment_list: Optional[str] = None
+    segment_list_type: Optional[str] = None
+    segment_time: Optional[float] = None
+    min_seg_duration: Optional[float] = None
 
 
 @dataclass
