e25f56e894fd9c1c314f38f000d4013f1fbf6f28
tumb1er <zimbler@gmail.com>
2025-01-10 11:58:13 +0300

    Configure stop signal for django-dev container

--- deploy/dvt/start-django-dev.sh
+++ deploy/dvt/start-django-dev.sh
@@ -3,4 +3,4 @@
 python /app/src/manage.py migrate --noinput && \
 python /app/src/manage.py createsuperuser --noinput 2>/dev/null || true && \
 
-python /app/src/manage.py runserver --noreload 0.0.0.0:8000
+exec python /app/src/manage.py runserver --noreload 0.0.0.0:8000
